4350202cbd56da596f4bb5aecc4fbf4cdd8317ad
KeisGSmit <60598021+KeisGSmit@users.noreply.github.com>
2021-03-24 14:06:28 +0100

    Added directories, started first basic app in vue introduction

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ReactAngularVue
+
+In this Repo I am exploring modern JavaScript frameworks.
+These include React, Vue and Angular. 
+This is not made to be deployed, but rather to document my education proccess. 
+
+## Technology Used
+- HTML
+- JavaScript
+- Angular
+- Vue
+- React
--- Vue_introduction/script.js
+++ Vue_introduction/script.js
@@ -1,31 +1,11 @@
 new Vue({
     el: '#app',
     data: {
-        todos:[],
-        userInput: '',
-
+        name: 'Keis'
     },
     methods: {
-        addToDo: function(){
-            if(this.userInput.trim() == ''){
-                return;
-            }
-            let newTodo = {
-                id: _.uniqueId(),
-                value: this.userInput
-            };
-            this.todos.push(newTodo);
-            console.log(this.todos)
-        },
-        removeToDo(todo){
-            let id = todo.id;
-            for(let i=0; i<this.todos.length;i++){
-                if(this.todos[i].id == id){
-                    this.todos.splice(i,1);
-                    break
-                }
-            }
+        nameChange: function(){
+            this.name = 'Anna';
         }
     }
-
 });
