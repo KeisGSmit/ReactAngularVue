a07b122897a9a6285b2fa5bfe506fab4fce781c0
KeisGSmit <60598021+KeisGSmit@users.noreply.github.com>
2021-03-24 12:29:26 +0100

    Added Vue

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
-let inputEl = $('input');
-let buttonEl = $('button');
-let ulEl = $('ul');
+new Vue({
+    el: '#app',
+    data: {
+        todos:[],
+        userInput: '',
 
-let todos = [];
-
-buttonEl.click(addToDo);
-
-function addToDo(){
-    let userInput = inputEl.val();
-
-    if(userInput.trim() == ''){
-        return;
-    }
-
-    let newTodo = {id: _.uniqueId(), value: userInput};
-
-    todos.push(newTodo);
-
-    $('<li>'+ userInput +'</li>')
-        .appendTo(ulEl)
-        .attr('dataset-id', newTodo.id)
-        .click(removeToDo);
-    console.log(todos);
-};
-
-function removeToDo(event){
-    var clickedLi = $(this);
-    var itemId = clickedLi.attr('dataset-id');
-
-    for(let i = 0; i<todos.length; i++){
-        if(todos[i].id == itemId){
-            todos.splice(i,1);
-            break;
+    },
+    methods: {
+        addToDo: function(){
+            if(this.userInput.trim() == ''){
+                return;
+            }
+            let newTodo = {
+                id: _.uniqueId(),
+                value: this.userInput
+            };
+            this.todos.push(newTodo);
+            console.log(this.todos)
+        },
+        removeToDo(todo){
+            let id = todo.id;
+            for(let i=0; i<this.todos.length;i++){
+                if(this.todos[i].id == id){
+                    this.todos.splice(i,1);
+                    break
+                }
+            }
         }
     }
 
-    clickedLi.remove();
-    console.log(todos);
-};
+});
