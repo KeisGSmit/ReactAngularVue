851dc76a42350d45173f16316b9158a91eff4da1
KeisGSmit <60598021+KeisGSmit@users.noreply.github.com>
2021-03-24 14:20:08 +0100

    outputting lists

--- Vue_introduction/script.js
+++ Vue_introduction/script.js
@@ -1,11 +1,15 @@
 new Vue({
     el: '#app',
     data: {
-        name: 'Keis'
+        name: 'Keis',
+        elements: []
     },
     methods: {
         nameChange: function(){
             this.name = 'Anna';
+        },
+        addElement: function(){
+            this.elements.push(this.elements.length + 1);
         }
     }
 });
